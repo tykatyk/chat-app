ee257efb827d9700792652bbe5bc684771f7e7ce
tykatyk <tykatyk@gmail.com>
2018-08-30 06:58:36 +0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
@@ -13,7 +13,37 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-})
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      "from": message.from,
+      "text": message.text,
+      "createdAt": new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   "from": message.from,
+    //   "text": message.text,
+    //   "createdAt": new Date().getTime()
+    // });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  })
+});
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
