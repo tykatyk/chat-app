d146bd695336d3521ae9a7d36823262aa99ecabd
tykatyk <tykatyk@gmail.com>
2018-09-04 06:35:17 +0300

    Add messages form and show messages to browser

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, cb) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    cb('message is correct');
     // socket.broadcast.emit('newMessage', {
     //   "from": message.from,
     //   "text": message.text,
