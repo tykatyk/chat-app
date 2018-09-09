54dea1633038df25c47cbfbde6dac272c32cf926
tykatyk <tykatyk@gmail.com>
2018-09-09 20:49:27 +0300

    Add css for a chat page

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, cb) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    cb('This is from the server.');
+    cb();
   });
 
   socket.on('createLocationMessage', function (coords) {
