d949ccf49deacf182fec8c35fe16de3761405017
tykatyk <tykatyk@gmail.com>
2018-09-15 17:07:44 +0300

    Add mustache.js for message templates

---
+++ playground/time.js
@@ -0,0 +1,20 @@
+// var moment = require('moment');
+//
+// var date = moment().format('hh:mm a');
+//
+// console.log(date);
+
+var date = new Date().getTime();
+
+console.log(date);
+
+// var obj = {
+//   func: function () {
+//     return function (text) {
+//       console.log(text);
+//     }
+//   }
+// }
+//
+// var test = obj.func();
+// test('Chris');
